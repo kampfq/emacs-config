97734594c6491ff7a941f580a3b75868ea1a75ab
Benjamin Issleib <ben@kampfq.eu>
2018-10-02 21:27:56 +0200

    fix init

--- init.el
+++ init.el
@@ -11,6 +11,12 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,12 +31,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; Bootstrap `use-package`
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
 
 ;; load all lisp files in the lisp directory, they contain custom stuff
 (mapc 'load (file-expand-wildcards "~/.emacs.d/lisp/*.el"))
