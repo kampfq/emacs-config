cc33a304207d3741081e86be4d3d4a9529d0ce5d
Benjamin Issleib <ben@kampfq.eu>
2018-10-03 11:38:13 +0200

    improvements to german language and cleanup

--- lisp/behaviour.el
+++ lisp/behaviour.el
@@ -1,3 +1,6 @@
 ;; Disable backup files
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
+
+;; write german umlauts when eg typing ae, switch with C-\
+(setq default-input-method 'german-postfix)
---
+++ lisp/latex.el
@@ -0,0 +1,5 @@
+(use-package auctex
+  :defer t
+  :ensure t
+  :config
+  (setq TeX-auto-save t))
