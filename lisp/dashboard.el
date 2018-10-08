3e15b179d44edf6b01d20c231ee3d8a085c46fef
Benjamin Issleib <bi@converia.de>
2018-10-08 11:09:42 +0200

    add undo tree

--- lisp/dashboard.el
+++ lisp/dashboard.el
@@ -4,3 +4,10 @@
   (dashboard-setup-startup-hook))
 
 (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+(setq dashboard-startup-banner 3)
+
+(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
---
+++ lisp/undo-tree.el
@@ -0,0 +1,6 @@
+(use-package undo-tree
+  :ensure t
+  :commands undo-tree-mode
+  :diminish undo-tree-mode
+  :config
+  (global-undo-tree-mode))
