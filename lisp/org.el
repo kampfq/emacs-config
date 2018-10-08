c74b651ac83e412af12f0d82700f2e685f320814
Benjamin Issleib <bi@converia.de>
2018-10-08 10:32:26 +0200

    fix org bullets

--- lisp/org.el
+++ lisp/org.el
@@ -1,2 +1,6 @@
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+;;(require 'org-bullets)
+;;(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(use-package org-bullets
+  :ensure t
+  :commands (org-bullets-mode)
+  :init (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
