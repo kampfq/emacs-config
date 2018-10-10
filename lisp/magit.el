8467e9e0e095d09cf7e2137c9e6c68db01d5cb4d
Benjamin Issleib <ben@kampfq.eu>
2018-10-10 07:51:28 +0200

    add ediff function

--- lisp/magit.el
+++ lisp/magit.el
@@ -6,3 +6,13 @@
   :ensure t
   :diminish git-gutter-mode
   :config (global-git-gutter-mode))
+
+;; add function to combine to commits when merging by pressinf "d" in ediff
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
